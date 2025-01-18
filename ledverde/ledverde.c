d132cea361cc659284b172f61b50c150a4de6b32
kaique <kaique@example.com>
2025-01-18 19:19:26 -0300

    adicionado o buzzer à tecla D

--- ledverde/ledverde.c
+++ ledverde/ledverde.c
@@ -2,8 +2,9 @@
 #include "pico/stdlib.h"
 
 #define LED_VERDE 13
+#define BUZZER 14 // Definição do pino do buzzer
 
-//mapa de teclas
+// Mapa de teclas
 char KEY_MAP[16] = {
     '1', '2' , '3', 'A',
     '4', '5' , '6', 'B',
@@ -11,24 +12,23 @@ char KEY_MAP[16] = {
     '*', '0' , '#', 'D'
 };
 
-//define os pinos do teclado com as portas GPIO
+// Define os pinos do teclado com as portas GPIO
 uint columns[4] = {4, 3, 2, 1}; 
 uint rows[4] = {8, 7, 6, 5};
-
 
 // Função para inicializar o teclado
 void init_teclado() {
     // Configura as colunas como saída
     for (int i = 0; i < 4; i++) {
-        gpio_init(columns[i]);  //inicializa as colunas
-        gpio_set_dir(columns[i], GPIO_OUT); // coloca as colunas como saída 
-        gpio_put(columns[i], 1); // Inicializa como HIGH = alto (1)
+        gpio_init(columns[i]);  // Inicializa as colunas
+        gpio_set_dir(columns[i], GPIO_OUT); // Configura como saída
+        gpio_put(columns[i], 1); // Inicializa como HIGH (1)
     }
 
     // Configura as linhas como entrada com pull-up
     for (int i = 0; i < 4; i++) {
-        gpio_init(rows[i]); // inicializa as linhas
-        gpio_set_dir(rows[i], GPIO_IN); // coloca as linhas como entrada 
+        gpio_init(rows[i]); // Inicializa as linhas
+        gpio_set_dir(rows[i], GPIO_IN); // Configura como entrada
         gpio_pull_up(rows[i]);
     }
 }
@@ -62,23 +62,33 @@ int main() {
     // Inicializa o teclado
     init_teclado();
 
+    // Inicializa os periféricos
+    gpio_init(LED_VERDE);
+    gpio_set_dir(LED_VERDE, GPIO_OUT);
+    gpio_init(BUZZER);
+    gpio_set_dir(BUZZER, GPIO_OUT);
+
     printf("Teclado inicializado.\n");
 
     char tecla;
 
-    gpio_init(LED_VERDE);
-    gpio_set_dir(LED_VERDE, GPIO_OUT);
-
     while (true) {
         // Detecta a tecla pressionada
-        gpio_put(LED_VERDE, 0); //mantém led verde desligado
+        gpio_put(LED_VERDE, 0); // Mantém o LED verde desligado
+        gpio_put(BUZZER, 0);    // Mantém o buzzer desligado
         tecla = detectar_tecla();
         if (tecla != '\0') {
-          printf("Tecla pressionada: %c\n", tecla);
-          if(tecla == 'A'){
-            gpio_put(LED_VERDE, 1);
-            printf("led verde ligado\n");
-          }
+            printf("Tecla pressionada: %c\n", tecla);
+            if (tecla == 'A') {
+                gpio_put(LED_VERDE, 1); // Liga o LED verde
+                printf("LED verde ligado\n");
+            } else if (tecla == 'D') {
+                gpio_put(BUZZER, 1); // Liga o buzzer
+                printf("Buzzer ativado\n");
+                sleep_ms(500);      // Buzzer ligado por 500 ms
+                gpio_put(BUZZER, 0); // Desliga o buzzer
+                printf("Buzzer desativado\n");
+            }
         }
 
         sleep_ms(100);
